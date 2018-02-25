7f743d5066d0f9c71a2972574e010612a4b43a71
Carlos Aya Moreno <carlosayam@users.noreply.github.com>
2018-02-25 23:36:59 +1100

    Minor corrections to kde launch script

--- python/steps/qsub_kde_ise_hd.sh
+++ python/steps/qsub_kde_ise_hd.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-$DIST=$1
-$NUM=$2
+DIST=$1
+NUM=$2
 
 declare -a START
 if [ $DIST == mul3 ]; then
-    if [ $NUM <= 512 ]; then
-      START=(0, 128, 256, 384)
+    if [ $NUM -le 512 ]; then
+      START=`seq 0 128 511`
       BLOCK=128
     else
       START=`seq 0 32 511`
       BLOCK=32
     fi
 else
-    if [ $NUM <= 512 ]; then
+    if [ $NUM -le 512 ]; then
       START[0]=0
       BLOCK=512
     else
-      START=(0, 128, 256, 384)
+      START=`seq 0 128 511`
       BLOCK=128
     fi
 fi
 
 for start in ${START[*]}; do
     cat << EOF > tmp.pbs
-    #PBS -N PLAN_$DIST_$NUM_$start
+    #PBS -N KDE_$DIST_$NUM_$start
     #PBS -l nodes=1:ppn=1
     #PBS -l vmem=4gb
     #PBS -l walltime=1:30:00
@@ -38,6 +38,7 @@ for start in ${START[*]}; do
     SW_DIR="$PBS_O_HOME/WDE/wde/python"
     . $SW_DIR/wdeenv3/bin/activate
     cd $SW_DIR
+    export PYTHONPATH=.
     python steps/kde_ise_hd.py $DIST $NUM $start $BLOCK
 EOF
 
